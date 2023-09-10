30ab6e0c0c036a444d25fd3f0befce0dddbbcf2b
Josiahalabi <josiahalabi@gmail.com>
2023-09-10 14:05:20 -0700

    six print

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - prints number from 0 to 9.
+ * main - Prints number from 0 to 9.
  *
- * Return; Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
