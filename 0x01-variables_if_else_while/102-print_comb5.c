0d9cd64c475fcadecfd71fe72a6a1eaed9c32601
Josiahalabi <josiahalabi@gmail.com>
2023-09-11 04:56:59 -0700

    one last digit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,4 +1,4 @@
-#include <stdio.h.
+#include <stdio.h.>
 
 /**
  * main - prints all possible combinations of  two-digit numbers
@@ -12,14 +12,14 @@ int main(void)
 	{
 		for (j = 0; j < 100; j++)
 		{
-			if (i , j)
+			if (i < j)
 			{
-				putchar((i /10) +48);
-				putchar((i % 10) +48);
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
 				putchar(' ');
-				putchar((j / 10) +48);
-				putchar((j % 10) +48);
-				if (i ! = 98 || j ! = 99)
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
 				{
 					putchar(',');
 					putchar(' ');
@@ -29,6 +29,4 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
-}
-						
-						
+}				
