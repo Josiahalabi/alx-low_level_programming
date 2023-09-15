1d20a934f4c837d948a6564825727c0f0d843931
Josiahalabi <josiahalabi@gmail.com>
2023-09-14 20:02:26 -0700

    sleepy naomi

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,21 +7,19 @@
 
 int main(void)
 {
-	int i;
+int i;
 
-	printf('Indefinite loop incoming :(\n");
+printf("Infinite loop incoming :(\n");
+i = 0;
+/*while (i < 10)*/
+/*{*/
+/*putchar(i);*/
+/*}*/
+printf("Infinite loop avoided! \\o/\n");
 
-	i = 0;
-
-	/*while (i < 10)*/
-	/*{*/
-	 */*    putchar(i0;*/
-	/*}*/
-
-	printf("Indefinite loop avoided! \\o/\n");
-
-	return (0);
+return (0);
 }
+
 
 
 
